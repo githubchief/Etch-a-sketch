dfd71f461f35abad5a5c9a69da8ee688eaf73ea9
githubchief <ranganathanshams@gmail.com>
2022-12-07 19:25:38 +0530

    add pentool to the canvas

--- main.js
+++ main.js
@@ -1,45 +1,79 @@
 //button obj to select grid size
 let gridButton = document.getElementById("grid-size");
 
-//obj for the grid container
+//cointainer obj for the grid container
 let container = document.getElementById("container");
 
-
+//default grid size
 createGrid(16);
+
 //event listener to get the prompt input from user when user clicks on grid-size button
 gridButton.addEventListener("click", handleGridSizeClick);
-
 
 //function to handle grid size button click
 function handleGridSizeClick(event) {
 
-
-    
     //prompt to get grid size
-    gridSize = prompt("Enter the grid size: between 16 and 99");
-    console.log(gridSize);
+    gridSize = prompt("Enter the grid size: ");
+    if (gridSize > 100 ) {
+        alert("Enter grid size less than 100");
+        //createGrid(16);
+        return;
+    }
     
     //call the function to create grid of size gridSize
     createGrid(gridSize);
   }
-  
+
+//function to create grid
 function createGrid(size) {
 
     removeChildren(container);
+
     gridCubeSize=(960/size)-2;
     let count=(size*size);
     for (let i=0; i<count;i++) {
         const square=document.createElement("div");
         square.classList.add(`square`);
-        square.style.width=`${gridCubeSize}px`;
-        square.style.height=`${gridCubeSize}px`;
-        container.appendChild(square);
+        (square).style.width=`${gridCubeSize}px`;
+        (square).style.height=`${gridCubeSize}px`;
+        container.appendChild((square));
     }
-    
+
+    container.childNodes.forEach(function(childElement) {
+    childElement.addEventListener("click", function() {
+        // Call the penTool function to start sketching
+        penTool();
+    });
+});    
 }
 
+let isPenToolActive = false;
 
+function penTool() {
+    if (isPenToolActive) {
+        // Stop the penTool function from being called on mouseover
+        container.childNodes.forEach(function(childElement) {
+            childElement.removeEventListener("mouseover", colorGrid);
+        });
+
+        // Set the flag variable to false to indicate that the penTool function is not active
+        isPenToolActive = false;
+    } else {
+
+          // Set the flag variable to true to indicate that the penTool function is active
+          isPenToolActive = true;
+        // Start the penTool function on mouseover
+        container.childNodes.forEach(function(childElement) {
+            childElement.addEventListener("mouseover", colorGrid);
+            
+        });
+    }
+}
+
+//function to remove the grid squares
 function removeChildren(element) {
+    //keep removing the child elements until zero childs are there in the container
     while (element.firstChild) {
       element.removeChild(element.firstChild);
     }
@@ -53,8 +87,23 @@ clearSignal.addEventListener("click", handleClearClick)
 
 //function to handle click clear button
 function handleClearClick(event) {
-
-    //change background-color of divs in the container to white
+    //change background-color of divs in the container to white rgb(238, 245, 245)
+        
+        container.childNodes.forEach(function(childElement) {
+            childElement.style.backgroundColor = 'rgb(238, 245, 245)';
+          });
     
+   
 }
 
+
+function colorGrid() {
+    // Generate a random color string in the format "rgb(x, y, z)"
+    let randomColor = "rgb(" + Math.floor(Math.random() * 256) + ", " +
+                               Math.floor(Math.random() * 256) + ", " +
+                               Math.floor(Math.random() * 256) + ")";
+  
+    // Use the `this` keyword to refer to the DOM element that the event
+    // was triggered on
+    this.style.backgroundColor = randomColor;
+  }
